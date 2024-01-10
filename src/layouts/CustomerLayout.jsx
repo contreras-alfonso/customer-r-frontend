2b97a373a802273e1bb43d94017f66bd784b7898
Alfonso <contrerasalfonso.lino@gmail.com>
2024-01-10 09:23:44 -0500

    Adding routes, creating CustomerHome, CustomerLayout, modifying index.css, creating the src structure.

---
+++ src/layouts/CustomerLayout.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CustomerLayout = () => {
+  return (
+    <div>CustomerLayout</div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
+import { CustomerLayout } from './layouts/CustomerLayout';
+import { CustomerHome } from './pages/CustomerHome';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <CustomerLayout />,
+    children: [
+      {
+        index: true,
+        element: <CustomerHome />,
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+     <RouterProvider router={router} />
   </React.StrictMode>,
 )
